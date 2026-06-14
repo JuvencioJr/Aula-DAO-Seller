19f71f018d5c9deda1a6fb2b76c0b4f41a8e8cb9
Nélton Juvencio <njuvencio@isced.ac.mz>
2026-06-14 23:26:21 +0200

    Project created

---
+++ src/Db/DBexeptionIntegrytDataBase.java
@@ -0,0 +1,13 @@
+package Db;
+
+public class DBexeptionIntegrytDataBase extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public DBexeptionIntegrytDataBase (String msg) {
+		super(msg);
+	}
+}
